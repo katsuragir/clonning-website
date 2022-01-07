b3689ea508b3901025ab806f147a617c62a2362f
Ridho Bagaskara <ridhobagaskara68@gmail.com>
2022-01-08 01:41:55 +0700

    crypto1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,20 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>PancakeSwap</title>
+        <base href="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
